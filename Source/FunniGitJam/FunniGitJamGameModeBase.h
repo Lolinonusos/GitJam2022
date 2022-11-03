288ee36e04e85b55e0527062e6f6425d78b94f24
Lolinonusos <89453125+Lolinonusos@users.noreply.github.com>
2022-11-03 09:05:34 +0100

    Unreal

---
+++ Source/FunniGitJam/FunniGitJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FunniGitJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUNNIGITJAM_API AFunniGitJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
